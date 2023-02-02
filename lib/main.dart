6f07c00e7fd73858d84a1916ca419e83f04ebcf9
Administrator <sabrisaeed9@gmail.com>
2023-02-02 14:01:53 +0200

    Finish Movie App

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'layout/home_layout/home_layout.dart';
 
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
